615f7391dd0528bdbb94bcb3c620d04245bbf7d0
Topias Syri <tpsy132@gmail.com>
2021-11-01 12:30:47 +0200

    Finished code

--- project3_type3_metaphors.py
+++ project3_type3_metaphors.py
@@ -9,21 +9,34 @@ import sys
 EXAMPLE_1 = "Yellow potato is scary"
 EXAMPLE_2 = "Whatever floats your fancy boat"
 
-# JJ = Adjective
-# RB = adverb, RBR, RBT, RN, RB?
-
 # nouns
 # NN NN$ NNS NNS$ NP NP$ NPS NPS$ NR NRS
 nouns = ["NN", "NN$", "NNS", "NNS$", "NP", 
     "NP$", "NPS", "NPS$", "NR", "NRS"]
 
+
+# manually picked from here:
+# https://provalisresearch.com/products/content-analysis-software/wordstat-dictionary/wordnet-based-categorization-dictionary/
+concrete_noun_categories = [
+    "animal",
+    "artifact",
+    "body",
+    "food",
+    "location"
+    "object",
+    "person",
+    "plant"
+]
+
+# JJ = Adjective
 adj = ["JJ"]
+# RB = adverb, RBR, RBT, RN, RB?
 adv = ["RB", "RBR", "RBT", "RN"]
 words = brown.words()
 tagged = brown.tagged_words()
 bcf = BigramCollocationFinder.from_words(words)
 
-def get_category(word: str, category=wn.NOUN) -> List:
+def get_synsets(word: str, category=wn.NOUN) -> List:
     """
     returns a list of categories for given word
     word is assumed to be a noun
@@ -33,52 +46,15 @@ def get_category(word: str, category=wn.NOUN) -> List:
 def tag_sentence(sentence: str):
     return pos_tag(sentence.split())
 
-def is_metaphor(sentence: str):
-    tags = tag_sentence(sentence)
-    senses = {}
-    noun_brown = {}
-    concretes = {}
-    for tag in tags:
-        if tag[1] in adj:
-            # Get all the different meanings of the adjective
-            s = wn.lemmas(tag[0])
-            if not s:
-                senses[tag[0]] = "UNKNOWN"
-            elif len(senses) == 1:
-                senses[tag[0]] = "NO METAPHOR"
-            else:
-                senses[tag[0]] = s
-        if tag[1] in nouns:
-            # Get the category of the noun
-            collocations = get_collocation(tag[0])
-            for k, v in collocations.items():
-                # Need to filter category here
-                category = get_category(k[0])
-                concretes[k[0]] = (v, category)
-
-        # sort k-vs by value and get first 3
-        sorted_concretes = [
-            (k, v) for k, v in sorted(
-                concretes.items(), key=lambda item: item[1][0]
-                )
-            ]
-        S1 = sorted_concretes[:3]
-
-        for adjective, sens in senses.items():
-            if sens == "UNKNOWN" or sens == "NO METAPHOR":
-                continue
-            for word, (v, synset) in S1:
-                for s in synset:
-                    # TODO: Needs "word.type.num" format...
-                    compatibility = s.wup_similarity(wn.synset(adjective))
-                    if compatibility >= 0.3:
-                        break
-            else:
-                # No compatibility found, is a metaphor
-                metaphor = True
-            # compatibility found, not a metaphor
-            metaphor = False
-            print(adjective, metaphor)
+def is_concrete_noun(synset: List):
+    """
+    Return true if any of the hypernyms are in concrete category
+    """
+    for hypernym_paths in synset.hypernym_paths():
+        for hypernym in hypernym_paths:
+            if hypernym.name().split(".")[0] in concrete_noun_categories:
+                return True
+    return False
 
 def get_collocation(word, threshold = 3):
     collocs = {}
@@ -90,15 +66,53 @@ def get_collocation(word, threshold = 3):
             collocs[w] = AB
     return collocs
 
+def is_metaphor(sentence: str):
+    tags = tag_sentence(sentence)
+    senses = {}
+    noun_brown = {}
+    concretes = {}
+    for tag in tags:
+        if tag[1] in adj:
+            # Get all the different meanings of the adjective
+            lemmas = wn.lemmas(tag[0])
+            if not lemmas:
+                senses[tag[0]] = "UNKNOWN"
+            elif len(senses) == 1:
+                senses[tag[0]] = "NO METAPHOR"
+            else:
+                senses[tag[0]] = lemmas
+        if tag[1] in nouns:
+            # Get the category of the noun
+            collocations = get_collocation(tag[0])
+            for k, v in collocations.items():
+                # Need to filter category here
+                for synset in get_synsets(k[0]):
+                    if is_concrete_noun(synset):
+                        # Store all concrete nouns in a dict
+                        concretes[synset] = v
 
-if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("usage: script.py <word> <type>")
-    elif sys.argv[2] == "noun":
-        print(get_category(str(sys.argv[1])))
-    elif sys.argv[2] == "adv":
-        print(get_category(str(sys.argv[1]), wn.ADV))
-    elif sys.argv[2] == "adj":
-        print(get_category(str(sys.argv[1]), wn.ADJ))
+    # sort dict by value and take the last 3
+    sorted_concretes = [
+        k for k, v in sorted(
+            concretes.items(), key=lambda item: item[1]
+            )
+        ]
+    S1 = sorted_concretes[3:]
+    if not S1:
+        print("No concrete nouns")
     else:
-        print("usage: script.py <word> <type>")
+        for adjective, lemmas in senses.items():
+            if lemmas == "UNKNOWN" or lemmas == "NO METAPHOR":
+                continue
+            for lemma in lemmas:
+                metaphor = False
+                for synset in S1:
+                    compatibility = synset.wup_similarity(lemma.synset())
+                    if compatibility >= 0.3:
+                        print("Not a metaphor")
+                        break
+                else:
+                    # No compatibility found, is a metaphor
+                    metaphor = True
+                    print(f"{sentence} has metaphor")
+                # compatibility found, not a metaphor
